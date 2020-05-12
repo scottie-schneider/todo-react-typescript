8aa2236685c0823390a0a4f190d55fae4a632b4f
scottie-schneider <scott@laclave.co>
2020-05-12 11:35:55 -0500

    displaying right todo based on redux state and id in param

--- src/IndividualTodo.tsx
+++ src/IndividualTodo.tsx
@@ -1,14 +1,31 @@
 import React, { Fragment, useState } from "react";
 import { RouteComponentProps } from "react-router-dom";
+import { connect } from "react-redux";
+
 import { Todo } from "./types";
 interface TodoProps extends RouteComponentProps<{ id: string }> {
   todos: Array<Todo>;
 }
 
-const IndividualTodo: React.FC<TodoProps> = (props: TodoProps) => {
-  const [name, setName] = useState("");
-  const [id, setId] = useState(props.match.params.id);
-  return <Fragment>todo {id}</Fragment>;
+const IndividualTodo: React.FC<TodoProps> = ({ todos, match }) => {
+  const [todoDetail, setTodoDetail] = useState(
+    todos.filter((x) => x.id === parseInt(match.params.id))[0]
+  );
+  return (
+    <Fragment>
+      <span>Todo Details</span>
+      <span>Name: {todoDetail.text}</span>
+      <span>
+        This todo is {todoDetail.complete ? "completed!" : "not completed :("}
+      </span>
+    </Fragment>
+  );
 };
 
-export default IndividualTodo;
+const mapStateToProps = (state: any) => {
+  return {
+    todos: state.todos,
+  };
+};
+
+export default connect(mapStateToProps)(IndividualTodo);
